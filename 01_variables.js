9a10678a924cb336409058c327b74613a1997237
SnehaSwarnkar <swarnkarsneha25@gmial.com>
2025-05-30 22:14:17 +0530

    02_datatypes

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "12345@gmial.com"
+var accountPassword = "12345"
+accountCity = "Indore" 
+// accountId = 2 
+accountEmail = "hxhc@gmail.com"
+accountPassword = "121212"
+accountCity = "Bengaluru"
+/*
+prefer not to use var 
+because of issue iin block scope and functional scope 
+*/
+
+//console.log(accountEmail)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+//alert(3+3)
+// we are using nodeJS not browser 
+console.log(3+3)
+
+console.log("Sneha")
+
+let name = "sneha"
+let age = 20
+let isLoggedIn = false
+
+// number => to power 53 
+// bigint
+// string =>""
+// boolean => true/fals
+// null => standalone value 
+// undefined => variable not assigned value 
+// symbol => unique value 
+
+// object 
+console.log(typeof age );
